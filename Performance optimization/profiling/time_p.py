718684ff97a15499bb6a0a7dcd0b5bafb1f4f6f7
gaur8126 <gaurlokesh1211@gmail.com>
2025-12-01 20:00:18 +0530

    time profiling

---
+++ Performance optimization/profiling/time_p.py
@@ -0,0 +1,38 @@
+import time 
+import logging
+from fastapi import FastAPI, Request
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] (line %(lineno)d) - %(levelname)s - %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S"
+)
+
+logger = logging.getLogger("profiler")
+
+app = FastAPI()
+
+
+@app.middleware('http')
+async def addd_timing(request:Request, call_next):
+    start_time = time.time()
+    response  = await call_next(request)
+    time_taken = time.time() - start_time
+    logger.info(f"Request to {request.url.path} took {time_taken:.3f} seconds.")
+    return response
+
+
+
+@app.get('/')
+def home():
+    return {'message': 'profile demo'}
+
+@app.get('/slow')
+async def slow_endpoint():
+    time.sleep(2)
+    return {'message':'This is a slow endpoint'}
+
+@app.get('/fast')
+async def fast_endpoint():
+    return {'message':'This is a fast endpoint.'}
